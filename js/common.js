84b32116768a900c8174f030f053cb6891260d00
deepchoco <lala87481@naver.com>
2022-05-18 18:04:29 +0900

    ㄴ
    ㄴ

--- js/common.js
+++ js/common.js
@@ -1,14 +1,16 @@
-function splitLocation(el){
-  let href = el.attr("href").split("/");
-  href = href[href.split-1].split(".");
-  href = href[0];
-  return href;
-}
-
-let currentHref = splitLocation($(location));
-
-$("#lnb a").each(function(){
-  if( splitLocation($(this)) == currentHref ) {
-    $(this).addClass("on");
+$(function(){
+  function splitLocation(el){
+    let href = el.attr("href").split("/");
+    href = href[href.length-1].split(".");
+    href = href[0];
+    return href;
   }
+
+  let currentHref = splitLocation($(location));
+
+  $("#lnb a").each(function(){
+    if( splitLocation($(this)) == currentHref ) {
+      $(this).addClass("on");
+    }
+  });
 });
